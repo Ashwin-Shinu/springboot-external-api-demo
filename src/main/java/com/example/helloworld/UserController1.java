1aa3c93c48389a1454ab117e6da629e7af0e6504
Ashwin Thomas Shinu (Intern) <ashwin@alfardancorp.net>
2026-08-14 10:26:44 +0400

    Added mock DELETE API for the user

--- src/main/java/com/example/helloworld/UserController1.java
+++ src/main/java/com/example/helloworld/UserController1.java
@@ -1,9 +1,7 @@
 package com.example.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.
+        *;
 
 @RestController
 public class UserController1 {
@@ -29,6 +27,13 @@ public class UserController1 {
     public String updateUser() {
 
         return "User updated successfully";
+
+    }
+    @DeleteMapping("/user")
+
+    public String deleteUser() {
+
+        return "User deleted successfully";
 
     }
 }
